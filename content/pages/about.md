48c33e0e4e09962e37a573d187ae6cc8cca3e094
Chinuno Usami <usami@chinuno.com>
2021-11-06 12:01:20 +0800

    new blog

---
+++ content/pages/about.md
@@ -0,0 +1,17 @@
++++
+title = "关于我"
+description = ""
+date = 2021-11-06 07:48:48+08:00
+path = "about"
+[taxonomies]
+categories = ["about"]
++++
+
+电子越共 / 一个魂 / osu! / 画渣 / GAMER! / 图形学小萌新 / 独立游戏开发者
+
+
+- Twitter：[@chinuno_usami](https://twitter.com/chinuno_usami)
+- Github：[chinuno-usami](https://github.com/chinuno-usami)
+- Telgram：[@chinuno_usami](https://t.me/chinuno_usami)
+
+
---
+++ new_post.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+# Copyright (c) 2017 Xu Shaohua<shaohua@biofan.org>. All rights reserved.
+# Use of this source is governed by General Public License that can be found
+# in the LICENSE file.
+
+set -xe
+
+# Create a new article template.
+
+ARTICLE=$1
+if [ -z "${ARTICLE}" ]; then
+	  echo "$0 article-name"
+	    exit 1
+fi
+TIMESTAMPE=$(date --rfc-3339=seconds)
+FILENAME="content/${ARTICLE}.md"
+URL_PATH="${ARTICLE}"
+echo "TIMESTMAP: ${TIMESTAMPE}"
+echo "FILENAME: ${FILENAME}"
+echo "URL_PATH: ${URL_PATH}"
+
+if [ ! -f "${FILENAME}" ]; then
+	  cat > "${FILENAME}" << EOF
++++
+title = "${ARTICLE}"
+description = ""
+date = ${TIMESTAMPE}
+path = "${URL_PATH}"
+[taxonomies]
+categories = []
++++
+
+<!-- more -->
+
+EOF
+
+fi
+
+vim "${FILENAME}"
